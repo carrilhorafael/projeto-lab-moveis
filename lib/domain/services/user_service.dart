a2e8353c9522257b4930431b0800d6b2de565efa
Eduardo Canellas <eduardocanellas98@gmail.com>
2021-11-30 12:57:13 -0300

    refactor user service

--- lib/domain/services/user_service.dart
+++ lib/domain/services/user_service.dart
@@ -4,27 +4,37 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'exceptions/not_found_exception.dart';
 
 class UserService {
+  static const collectionName = "users";
   final FirebaseFirestore store;
 
   UserService(this.store);
 
   Future<void> create(User user) {
-    var users = this.store.collection("users");
+    var users = this.store.collection(collectionName);
     return users.add(user.toMap()).then((value) => {user.id = value.id});
   }
 
   Future<User> find(String id) async {
-    final snapshot = await this.store.collection("users").doc(id).get();
-    if (!snapshot.exists) {
-      throw NotFoundException();
-    }
-    final user = User.fromMap(snapshot.data()!);
-    user.id = id;
+    final snapshot = await _fetchSnapshot(id);
+    final user = snapshot.data()!;
+    user.id = snapshot.id;
+
     return user;
   }
 
-  void update(User user) {
-    throw new UnimplementedError();
+  CollectionReference<User> _collection() {
+    return this.store.collection(collectionName).withConverter<User>(
+        fromFirestore: (snapshot, _) {
+          if (!snapshot.exists) {
+            throw NotFoundException();
+          }
+          return User.fromMap(snapshot.data()!);
+        },
+        toFirestore: (user, _) => user.toMap());
+  }
+
+  Future<DocumentSnapshot<User>> _fetchSnapshot(String id) async {
+    return await _collection().doc(id).get();
   }
 
   void delete(String id) {
