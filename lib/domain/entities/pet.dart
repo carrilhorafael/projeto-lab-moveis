ede27169e11ccfc06e2fca23c267c9651d74b7da
Eduardo Canellas <eduardocanellas98@gmail.com>
2021-12-07 09:42:31 -0300

    make pet JSON serializable

--- lib/domain/entities/pet.dart
+++ lib/domain/entities/pet.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Pet {
   String id;
   String ownerId;
@@ -9,7 +11,7 @@ class Pet {
   int age;
 
   Pet({
-    required this.id,
+    this.id = "",
     required this.ownerId,
     required this.name,
     this.description = "",
@@ -18,6 +20,41 @@ class Pet {
     required this.size,
     required this.age,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      if (id != "") 'id': id,
+      'ownerId': ownerId,
+      'name': name,
+      'description': description,
+      'species': species,
+      'race': race,
+      'size': size.toString().split('.')[1],
+      'age': age,
+    };
+  }
+
+  factory Pet.fromMap(Map<String, dynamic> map) {
+    return Pet(
+      id: map['id'] ?? "",
+      ownerId: map['ownerId'],
+      name: map['name'],
+      description: map['description'],
+      species: map['species'],
+      race: map['race'],
+      size: enumFromString(Size.values, map['size']),
+      age: map['age'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Pet.fromJson(String source) => Pet.fromMap(json.decode(source));
 }
 
 enum Size { small, medium, big }
+
+// https://stackoverflow.com/questions/27673781/enum-from-string/44060511
+T enumFromString<T>(List<T> values, String value) {
+  return values.firstWhere((v) => v.toString().split('.')[1] == value);
+}
