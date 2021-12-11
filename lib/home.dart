a4613a5f3b46532c22c66b5284053a17eb852b46
BunnyMerz <tguerreirinhos.polo900@gmail.com>
2021-12-11 17:27:24 -0300

    Solved sdk version and build gradle version

--- lib/home.dart
+++ lib/home.dart
@@ -22,19 +22,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -77,8 +64,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => SearchPage()),
-            );
-            },
+                );
+              },
             )
           ],
         ),
--- lib/util/geo.dart
+++ lib/util/geo.dart
@@ -8,7 +8,7 @@ import 'package:geolocator/geolocator.dart';
 ///
 /// When the location services are not enabled or permissions
 /// are denied the `Future` will return an error.
-Future<Position> _determinePosition() async {
+Future<Position> determinePosition() async {
   bool serviceEnabled;
   LocationPermission permission;
 
