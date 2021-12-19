cf7d8fb748d3c49b366141b9c81a503cea952da7
EC <eduardocanellas98@gmail.com>
2021-12-19 10:58:34 -0300

    Home screen (#5)
    * inicializar branch
    
    * home_screen
    
    * refactor: split test page from home page
    
    * insert test page when in debug mode
    
    * run fmt
    
    Co-authored-by: ricardoasantos91 <ricardoasantos91@gmail.com>

---
+++ lib/screens/test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:projeto_lab/screens/home.dart';
+
+import 'package:projeto_lab/screens/search_settings.dart';
+
+import '../auth.dart';
+
+class TestPage extends StatelessWidget {
+  TestPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(
+              child: Text('Fluxo normal do App'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => HomePage(title: "PeTinder")),
+                );
+              },
+            ),
+            TextButton(
+              child: Text('Testar configurações de busca'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SearchPage()),
+                );
+              },
+            ),
+            TextButton(
+                child: Text('Testar login'),
+                onPressed: () {
+                  login("bruno@email.com", "supernenechi").then(print);
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
