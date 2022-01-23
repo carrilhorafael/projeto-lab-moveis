639f4fd3c96ff5cf7c0e9c5a4236915a26c5728b
Eduardo Canellas <eduardocanellas98@gmail.com>
2022-01-23 16:05:59 -0300

    feat: implement chat service

--- lib/domain/entities/message.dart
+++ lib/domain/entities/message.dart
@@ -1,16 +1,46 @@
+import 'dart:convert';
+
 import 'package:projeto_lab/domain/entities/user.dart';
 
 class Message {
   String interestId;
   String senderId = "";
   String content;
+  late DateTime createdAt;
 
   Message({
     required this.interestId,
     required this.content,
-  });
+  }) {
+    createdAt = DateTime.now();
+  }
 
   bool isFrom(User user) {
     return senderId == user.id;
   }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'interestId': interestId,
+      'senderId': senderId,
+      'content': content,
+      'createdAt': createdAt,
+    };
+  }
+
+  factory Message.fromMap(Map<String, dynamic> map) {
+    final message = Message(
+      interestId: map['interestId'] ?? '',
+      content: map['content'] ?? '',
+    );
+    message.senderId = map['senderId'] ?? '';
+    message.createdAt = map['createdAt'];
+
+    return message;
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Message.fromJson(String source) =>
+      Message.fromMap(json.decode(source));
 }
--- lib/domain/services/chat_service.dart
+++ lib/domain/services/chat_service.dart
@@ -1,10 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:projeto_lab/domain/entities/Interest.dart';
 import 'package:projeto_lab/domain/entities/message.dart';
+import 'package:projeto_lab/domain/services/interest_service.dart';
 
-// NOTE this service will probably change after we start implementing the chat itself
+import 'exceptions/not_found_exception.dart';
 
-abstract class ChatService {
-  List<Message> messages(Interest interest);
+class ChatService {
+  final InterestService interestService;
+  ChatService(this.interestService);
 
-  void send(Message message);
+  CollectionReference<Message> collection(String interestId) {
+    return interestService
+        .collection()
+        .doc(interestId)
+        .collection("messages")
+        .withConverter(
+            fromFirestore: (snapshot, _) {
+              if (!snapshot.exists) {
+                throw NotFoundException();
+              }
+              final map = snapshot.data()!;
+              map['id'] = snapshot.id;
+              return Message.fromMap(map);
+            },
+            toFirestore: (message, _) => message.toMap());
+  }
+
+  /// `listener` receives new messages. Useful e.g. to update state after receiving a new message.
+  Future<List<Message>> messages(
+      Interest interest, Function(Message) listener) async {
+    final query = collection(interest.id).orderBy("createdAt");
+
+    final messages = await query.get();
+
+    query.limit(1).snapshots().listen((event) {
+      listener(event.docChanges.first.doc.data()!);
+    });
+
+    return messages.docs.map((e) => e.data()).toList();
+  }
+
+  Future<void> send(Message message) {
+    return collection(message.interestId).add(message);
+  }
 }
